a35158f3dd90ba8ba802bece7b791929aa86c1f7
unknown <gagas112212@gmail.com>
2024-03-07 13:21:43 +0700

    migration:users & wallet

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,8 +14,12 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->boolean("verified")->nullable();
+            $table->string("profile_picuture");
+            $table->string("ktp");
             $table->rememberToken();
             $table->timestamps();
         });
